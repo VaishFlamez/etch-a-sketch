2cbf41dabce1128dfae8810a26445dd4582a01c3
Vaishak <vaishakbarca12@gmail.com>
2021-11-25 00:10:38 +0530

    rainbow coloring added

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
 
 // TO CHANGE
-// ADD RGB - RAINBOW COLOR FEATURE
 // ADD SLIDER AND FIX THE PIXEL THING
+
+
+
 
 const gridContainer = document.getElementById("gridContainer");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("gridCell");
+let colorPicker = document.getElementById("colorPicker");
+let gridSlider = document.getElementById("gridSlider");
+let currentColor = "#333333";
 
-let currentMode = "color"
 
+let currentMode = "color";
 
-let currentGridSize = prompt("Enter Grid Size")
+let currentGridSize = 20;
 window.onload = () => createGrid(currentGridSize);
 
 
 let colorModeBtn = document.getElementById("colorModeBtn");
 let eraserModeBtn = document.getElementById("eraserModeBtn");
+let rainbowModeBtn = document.getElementById("rainbowModeBtn");
 let clearBtn = document.getElementById("clearBtn");
-
 
 colorModeBtn.onclick = () => currentMode = "color";
 eraserModeBtn.onclick = () => currentMode = "eraser";
+rainbowModeBtn.onclick = () => currentMode = "rainbow";
 clearBtn.addEventListener("click", reloadGrid);
+colorPicker.onchange = (e) => setCurrentColor(e.target.value);
+// gridSlider.onmousemove = (e) => currentGridSize = e.target.value;
+gridSlider.onchange = (e) => updateGridSize(e.target.value);
 
+function setCurrentColor(newColor) {
+    currentColor = newColor;
+}
 
 function reloadGrid() {
-
     gridContainer.innerHTML = " ";
     createGrid(currentGridSize);
 }
 
 
+function updateGridSize(value) {
+    setCurrentSize(value);
+    reloadGrid();
+}
 
-
-
+function setCurrentSize(newSize) {
+    currentGridSize = newSize;
+}
 
 function createGrid(size) {
 
@@ -55,18 +71,22 @@ function makeCells(size) {
             let newCell = document.createElement("div");
             rows[j].appendChild(newCell).className = "gridCell";
             newCell.addEventListener("mouseover", changeColor);
-            // newCell.addEventListener("mouseout", resetColor);
 
         }
     }
-
-
 }
 
 
 function changeColor(e) {
     if (currentMode == "color")
-        e.target.style.backgroundColor = "#fe3a00";
+        e.target.style.backgroundColor = currentColor;
     else if (currentMode == "eraser")
         e.target.style.backgroundColor = "#ffffff";
+    else if (currentMode == "rainbow") {
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    }
+
 }
